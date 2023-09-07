770a886d34b075b84ca21adedf27fbe48e3de030
head1ton <head1ton@gmail.com>
2023-09-08 01:22:43 +0900

    9. ProductService class 생성

--- src/test/java/ai/springtest/product/ProductServiceTest.java
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -21,17 +21,21 @@ public class ProductServiceTest {
         productService.addProduct(request);
     }
 
-    private enum DiscountPolicy {
-        NONE
-
-    }
-
     private record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
-
         private AddProductRequest {
             Assert.hasText(name, "상품명은 필수입니다.");
             Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
             Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
         }
+
+    }
+
+    private enum DiscountPolicy {
+        NONE
+
+    }
+
+    private class ProductService {
+
     }
 }
