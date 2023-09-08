b650acb322638f1c0be966437e788e1a65998e04
Hwan <head1ton@gmail.com>
2023-09-09 05:26:49 +0900

    Jpa로 변환 (#11)

--- src/main/java/ai/springtest/payment/repository/PaymentRepository.java
+++ src/main/java/ai/springtest/payment/repository/PaymentRepository.java
@@ -3,16 +3,8 @@ package ai.springtest.payment.repository;
 import ai.springtest.payment.domain.Payment;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class PaymentRepository {
-
-    private final Map<Long, Payment> persistence = new HashMap<>();
-    private Long sequence = 1L;
-
-    public void save(final Payment payment) {
-        payment.assignId(++sequence);
-        persistence.put(payment.getId(), payment);
-    }
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
