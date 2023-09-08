496dda8aedb8356373be5dc2ead03eefa3ee7d6e
head1ton <head1ton@gmail.com>
2023-09-08 22:18:03 +0900

    5. test를 위해 상품 등록

--- src/test/java/ai/springtest/order/OrderServiceTest.java
+++ src/test/java/ai/springtest/order/OrderServiceTest.java
@@ -2,6 +2,8 @@ package ai.springtest.order;
 
 import ai.springtest.order.controller.OrderService;
 import ai.springtest.order.dto.CreateOrderRequest;
+import ai.springtest.product.ProductSteps;
+import ai.springtest.product.controller.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,15 @@ public class OrderServiceTest {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private ProductService productService;
 
     @Test
     @DisplayName("상품주문")
     public void 상품주문() {
+        // 상품 등록
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+
         final CreateOrderRequest request = OrderSteps.상품주문요청_생성();
 
         orderService.createOrder(request);
