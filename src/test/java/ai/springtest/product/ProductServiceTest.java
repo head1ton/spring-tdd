699ff7ebaa3babed244f64bda05a06dfa777d1f6
head1ton <head1ton@gmail.com>
2023-09-08 11:44:33 +0900

    1. 조회 기능 구현을 위해 test case 작성

---
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -0,0 +1,19 @@
+package ai.springtest.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+
+    @Test
+    @DisplayName("상품조회")
+    public void 상품조회() {
+        // 등록
+
+        // 조회
+
+        // 검증
+
+    }
+}
