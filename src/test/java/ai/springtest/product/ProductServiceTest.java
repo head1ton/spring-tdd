f7fdb5ccfc56693dbfb3882842a88d4404bf5c27
head1ton <head1ton@gmail.com>
2023-09-08 16:10:33 +0900

    1. 테스트 코드 작성 완료

--- src/test/java/ai/springtest/product/ProductServiceTest.java
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -13,32 +13,34 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
+    @Autowired
     private ProductPort productPort;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     @DisplayName("상품수정")
     public void 상품수정() {
+        // 상품 등록
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000,
             DiscountPolicy.NONE);
         Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
 
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
-
         productService.updateProduct(productId, request);
 
-        assertThat(product.getName()).isEqualTo("상품 수정");
-        assertThat(product.getPrice()).isEqualTo(2000);
+        // 조회
+        GetProductResponse response = productService.getProduct(productId);
+
+        assertThat(response.name()).isEqualTo("상품 수정");
+        assertThat(response.price()).isEqualTo(2000);
     }
 }
