d3e21f22f763eb9d1f9cb23541a4bcb4103fb93c
head1ton <head1ton@gmail.com>
2023-09-09 05:26:18 +0900

    Jpa로 변환

--- src/main/java/ai/springtest/payment/domain/Payment.java
+++ src/main/java/ai/springtest/payment/domain/Payment.java
@@ -1,25 +1,35 @@
 package ai.springtest.payment.domain;
 
 import ai.springtest.order.domain.Order;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
 @Getter
+@Entity
+@Table(name = "payment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
 
-    private final Order order;
-    private final String cardNumber;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
+    private Order order;
+    private String cardNumber;
 
     public Payment(final Order order, final String cardNumber) {
         Assert.notNull(order, "주문은 필수입니다.");
         Assert.hasText(cardNumber, "카드 번호는 필수입니다.");
         this.order = order;
         this.cardNumber = cardNumber;
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
     }
 
     public int getPrice() {
--- src/main/java/ai/springtest/payment/service/PaymentAdapter.java
+++ src/main/java/ai/springtest/payment/service/PaymentAdapter.java
@@ -1,27 +1,32 @@
 package ai.springtest.payment.service;
 
 import ai.springtest.order.domain.Order;
+import ai.springtest.order.respository.OrderRepository;
 import ai.springtest.payment.domain.Payment;
 import ai.springtest.payment.repository.PaymentRepository;
 import ai.springtest.product.domain.Product;
 import ai.springtest.product.enums.DiscountPolicy;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class PaymentAdapter implements PaymentPort {
 
     private final PaymentGateway paymentGateway;
     private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
 
     public PaymentAdapter(final PaymentGateway paymentGateway,
-        final PaymentRepository paymentRepository) {
+        final PaymentRepository paymentRepository, final OrderRepository orderRepository) {
         this.paymentGateway = paymentGateway;
         this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public Order getOrder(final Long id) {
-        return new Order(new Product("상품1", 1000, DiscountPolicy.NONE), 1);
+        return orderRepository.findById(id)
+                              .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
     }
 
     @Override
