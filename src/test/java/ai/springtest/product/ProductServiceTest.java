65781f48b51f2c823cbdfd45aff691e7a7f4a15e
head1ton <head1ton@gmail.com>
2023-09-08 11:51:07 +0900

    7. Record로 변환

--- src/test/java/ai/springtest/product/ProductServiceTest.java
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -23,23 +23,14 @@ public class ProductServiceTest {
         assertThat(response).isNotNull();
     }
 
-    private class GetProductResponse {
+    private record GetProductResponse(Long id, String name, int price,
+                                      DiscountPolicy discountPolicy) {
 
-        private final Long id;
-        private final String name;
-        private final int price;
-        private final DiscountPolicy discountPolicy;
-
-        public GetProductResponse(final Long id, final String name, final int price,
-            final DiscountPolicy discountPolicy) {
+        private GetProductResponse {
             Assert.notNull(id, "상품 ID는 필수입니다.");
             Assert.hasText(name, "상품명은 필수입니다.");
             Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
             // 가격은 할인 받으면 0원 일수도 있으니 체크를 빼고
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.discountPolicy = discountPolicy;
         }
     }
 }
