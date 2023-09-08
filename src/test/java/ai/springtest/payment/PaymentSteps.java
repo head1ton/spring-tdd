c615b1d3c1c31fff6f189ea2c86817d702ce4e91
Hwan <head1ton@gmail.com>
2023-09-09 05:18:11 +0900

    Api test 완료 (#10)

--- src/test/java/ai/springtest/payment/PaymentSteps.java
+++ src/test/java/ai/springtest/payment/PaymentSteps.java
@@ -1,6 +1,10 @@
 package ai.springtest.payment;
 
 import ai.springtest.payment.dto.PaymentRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
 
 public class PaymentSteps {
 
@@ -8,5 +12,16 @@ public class PaymentSteps {
         final Long orderId = 1L;
         final String cardNumber = "1234-1234-1234-1234";
         return new PaymentRequest(orderId, cardNumber);
+    }
+
+    static ExtractableResponse<Response> 주문결제요청(final PaymentRequest request) {
+        return RestAssured.given().log().all()
+                          .contentType(
+                              MediaType.APPLICATION_JSON_VALUE)
+                          .body(request)
+                          .when()
+                          .post("/payments")
+                          .then().log().all()
+                          .extract();
     }
 }
