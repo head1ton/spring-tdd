759eaec662addf70b6032a2368090d7820e9684b
head1ton <head1ton@gmail.com>
2023-09-09 00:58:09 +0900

    2. OrderSteps 로 이동

--- src/test/java/ai/springtest/order/OrderServiceTest.java
+++ src/test/java/ai/springtest/order/OrderServiceTest.java
@@ -4,15 +4,10 @@ import static ai.springtest.order.OrderSteps.상품주문요청_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import ai.springtest.ApiTest;
-import ai.springtest.order.dto.CreateOrderRequest;
 import ai.springtest.product.ProductSteps;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 
 class OrderApiTest extends ApiTest {
@@ -22,16 +17,9 @@ class OrderApiTest extends ApiTest {
     public void 상품주문() {
 
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
-        final CreateOrderRequest request = 상품주문요청_생성();
+        final var request = 상품주문요청_생성();
 
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                                                                  .contentType(
-                                                                      MediaType.APPLICATION_JSON_VALUE)
-                                                                  .body(request)
-                                                                  .when()
-                                                                  .post("/orders")
-                                                                  .then().log().all()
-                                                                  .extract();
+        final var response = OrderSteps.상품주문요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
--- src/test/java/ai/springtest/order/OrderSteps.java
+++ src/test/java/ai/springtest/order/OrderSteps.java
@@ -1,6 +1,10 @@
 package ai.springtest.order;
 
 import ai.springtest.order.dto.CreateOrderRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
 
 public class OrderSteps {
 
@@ -8,5 +12,16 @@ public class OrderSteps {
         final Long productId = 1L;
         final int quantity = 2;
         return new CreateOrderRequest(productId, quantity);
+    }
+
+    static ExtractableResponse<Response> 상품주문요청(final CreateOrderRequest request) {
+        return RestAssured.given().log().all()
+                          .contentType(
+                              MediaType.APPLICATION_JSON_VALUE)
+                          .body(request)
+                          .when()
+                          .post("/orders")
+                          .then().log().all()
+                          .extract();
     }
 }
