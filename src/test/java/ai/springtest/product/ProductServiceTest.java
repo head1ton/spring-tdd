dccf4a0068c806e19dc26640eb68e384520803cc
head1ton <head1ton@gmail.com>
2023-09-08 11:50:42 +0900

    6. Assert 추가

--- src/test/java/ai/springtest/product/ProductServiceTest.java
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ai.springtest.product.enums.DiscountPolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 public class ProductServiceTest {
 
@@ -31,6 +32,10 @@ public class ProductServiceTest {
 
         public GetProductResponse(final Long id, final String name, final int price,
             final DiscountPolicy discountPolicy) {
+            Assert.notNull(id, "상품 ID는 필수입니다.");
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+            // 가격은 할인 받으면 0원 일수도 있으니 체크를 빼고
             this.id = id;
             this.name = name;
             this.price = price;
