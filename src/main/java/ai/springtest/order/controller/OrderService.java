d7f974e114c8bca4eb362550f95bc36e6865db63
Hwan <head1ton@gmail.com>
2023-09-09 00:31:53 +0900

    API test 로 변환 (#6)

--- src/main/java/ai/springtest/order/controller/OrderService.java
+++ src/main/java/ai/springtest/order/controller/OrderService.java
@@ -4,9 +4,17 @@ import ai.springtest.order.dto.CreateOrderRequest;
 import ai.springtest.order.service.OrderPort;
 import ai.springtest.order.domain.Order;
 import ai.springtest.product.domain.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/orders")
 public class OrderService {
 
     private final OrderPort orderPort;
@@ -15,10 +23,14 @@ public class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(final CreateOrderRequest request) {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> createOrder(@RequestBody final CreateOrderRequest request) {
         Product product = orderPort.getProductById(request.productId());
         Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/test/java/ai/springtest/order/OrderServiceTest.java
+++ src/test/java/ai/springtest/order/OrderServiceTest.java
@@ -1,31 +1,43 @@
 package ai.springtest.order;
 
+import static ai.springtest.order.OrderSteps.상품주문요청_생성;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ai.springtest.ApiTest;
 import ai.springtest.order.controller.OrderService;
 import ai.springtest.order.dto.CreateOrderRequest;
 import ai.springtest.product.ProductSteps;
 import ai.springtest.product.controller.ProductService;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
-public class OrderServiceTest {
 
-    @Autowired
-    private OrderService orderService;
-    @Autowired
-    private ProductService productService;
+class OrderApiTest extends ApiTest {
 
     @Test
     @DisplayName("상품주문")
     public void 상품주문() {
-        // 상품 등록
-        productService.addProduct(ProductSteps.상품등록요청_생성());
 
-        final CreateOrderRequest request = OrderSteps.상품주문요청_생성();
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        final CreateOrderRequest request = 상품주문요청_생성();
 
-        orderService.createOrder(request);
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                                                                  .contentType(
+                                                                      MediaType.APPLICATION_JSON_VALUE)
+                                                                  .body(request)
+                                                                  .when()
+                                                                  .post("/orders")
+                                                                  .then().log().all()
+                                                                  .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
 }
