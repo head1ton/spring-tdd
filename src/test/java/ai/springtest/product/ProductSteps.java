4a54e02be7038d9bcd5f722155c26d1fa6a3c106
head1ton <head1ton@gmail.com>
2023-09-08 11:53:55 +0900

    9. class 로 빼서 공용으로 사용할 수 있게

---
+++ src/test/java/ai/springtest/product/ProductSteps.java
@@ -0,0 +1,29 @@
+package ai.springtest.product;
+
+import ai.springtest.product.dto.AddProductRequest;
+import ai.springtest.product.enums.DiscountPolicy;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+public class ProductSteps {
+
+    public static AddProductRequest 상품등록요청_생성() {
+        String name = "상품명";
+        int price = 1000;
+        final AddProductRequest request = new AddProductRequest(name, price, DiscountPolicy.NONE);
+        return request;
+    }
+
+    public static ExtractableResponse<Response> 상품등록요청(
+        final AddProductRequest request) {
+        return RestAssured.given().log().all()
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .body(request)
+                          .when()
+                          .post("/products")
+                          .then()
+                          .log().all().extract();
+    }
+}
