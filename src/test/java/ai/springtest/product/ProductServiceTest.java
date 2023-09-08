5ce5153b8b0f343960f94365b423dd7ec32c141d
head1ton <head1ton@gmail.com>
2023-09-08 14:27:51 +0900

    2. UpdateProductRequest DTO record로 생성

--- src/test/java/ai/springtest/product/ProductServiceTest.java
+++ src/test/java/ai/springtest/product/ProductServiceTest.java
@@ -1,5 +1,27 @@
 package ai.springtest.product;
 
+import ai.springtest.product.enums.DiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
 public class ProductServiceTest {
 
+    @Test
+    @DisplayName("상품수정")
+    public void 상품수정() {
+        final Long productId = 1L;
+        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000,
+            DiscountPolicy.NONE);
+        productService.updateProduct(productId, request);
+    }
+
+    private record UpdateProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+
+        private UpdateProductRequest {
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+        }
+    }
 }
